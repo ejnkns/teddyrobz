9248dcc107a771306f5cc2ffae7949056ce6f827
Elliot Jenkins <elliotjenkins3@gmail.com>
2019-08-21 17:22:25 +0900

    better video background, added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import YouTube from 'react-youtube';
+import IcoMoon from 'react-icomoon';
+import ReactPlayer from 'react-player';
 import './App.css';
 //import backgroundvid from './tokyotrain.webm';
 
 function App() {
+	return (
+		<div className="App">
+			<div className="video-background">
+				<div className="video-foreground">
+					<Player/>
+				</div>
+			</div>
+			<h1>teddyrobz</h1>
+			<footer>
+				<a className="ig" href="https://instagram.com/teddyrobz" title="TeddyRobz on Instagram"
+				   target="_blank">
+					<IcoMoon icon="instagram"/>
+				</a>
+				<a className="yt" href="https://www.youtube.com/channel/UCzTojQgbHiuAjaub3ePvNKw" title="TeddyRobz on YouTube" target="_blank">
+					<IcoMoon icon="youtube"/>
+				</a>
+			</footer>
+		</div>
+	);
+}
+
+class Player extends React.Component {
+	videoUrl = "htttps://www.youtube.com/watch?v=UExPgSxXtOc";
+	/*
 	const options = {
-		height: '390',
-		width: '640',
 		playerVars: {
 			autoplay: 1,
 			controls: 0,
@@ -14,21 +37,42 @@ function App() {
 			fs: 0,
 			iv_load_policy: 3,
 			loop: 1,
-			modestbranding: 1,
+			modestbranding: 1
 		}
 	};
+	*/
 
-	return (
-		<div className="App">
-			<YouTube
-				videoId="UExPgSxXtOc"
-				opts={options}
-			/>
-			<p>
-				teddyrobz
-			</p>
-		</div>
-	);
+	/*
+	function replay(event) {
+		function onPlayerStateChange(event) {
+			if (event.data == YT.PlayerState.ENDED) {
+				player.seekTo(0);
+				player.playVideo();
+			}
+		}
+	}
+	*/
+
+	render () {
+		return (
+			<div className='player-wrapper'>
+				<ReactPlayer
+					ref={player => { this.player = player }}
+					url={this.videoUrl}
+					className='react-player'
+					playing
+					width='100%'
+					height='100%'
+					progressInterval={100}
+					onProgress={progress => {
+						if (progress.played >= 0.99) {
+							this.player.seekTo(0)
+						}
+					}}
+				/>
+			</div>
+		)
+	}
 }
 
 export default App;
